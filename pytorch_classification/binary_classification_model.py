0934cedfb7df69c9a1af776d902817571cba3ca2
Abdul Rashed Jamshidi <ajamshid@f4r6s8.clusters.42paris.fr>
2026-03-01 19:34:40 +0100

    binary classificaion model enhanced

--- pytorch_classification/binary_classification_model.py
+++ pytorch_classification/binary_classification_model.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import torch
 from torch import nn
+import pandas as pd
+import numpy as np
 
 
 n_samples = 1000
@@ -14,7 +16,6 @@ X, y = make_circles(n_samples,
                     random_state=42) # keep random state so we get the same values each time we lauch the programe
 
 # Make DataFrame of circle data
-import pandas as pd
 circles = pd.DataFrame({"X1": X[:, 0],
     "X2": X[:, 1],
     "label": y
@@ -29,8 +30,8 @@ plt.scatter(x=X[:, 0],
             cmap=plt.cm.RdYlBu)
 plt.show()
 
-y = torch.from_numpy(y)
-X = torch.from_numpy(X)
+y = torch.from_numpy(y).float()
+X = torch.from_numpy(X).float()
 
 X_train, X_test, y_train, y_test = train_test_split(X, 
                                                     y, 
@@ -41,18 +42,101 @@ class CircleModelV0(nn.Module):
     def __init__(self):
         super().__init__()
         # 2. Create 2 nn.Linear layers capable of handling X and y input and output shapes
-        self.layer_1 = nn.Linear(in_features=2, out_features=5) # takes in 2 features (X), produces 5 features
-        self.layer_2 = nn.Linear(in_features=5, out_features=1) # takes in 5 features, produces 1 feature (y)
-    
+        self.layer_1 = nn.Linear(in_features=2, out_features=10)
+        self.layer_2 = nn.Linear(in_features=10, out_features=10)# takes in 2 features (X), produces 5 features
+        self.layer_3 = nn.Linear(in_features=10, out_features=1) # takes in 5 features, produces 1 feature (y)
+        self.relu = nn.ReLU()
     # 3. Define a forward method containing the forward pass computation
     def forward(self, x):
         # Return the output of layer_2, a single feature, the same shape as y
-        return self.layer_2(self.layer_1(x)) # computation goes through layer_1 first then the output of layer_1 goes through layer_2
-
+        # return self.layer_2(self.relu(self.layer_1(x))) # computation goes through layer_1 first then the output of layer_1 goes through layer_2
+        return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
 # 4. Create an instance of the model and send it to target device
 model_0 = CircleModelV0()
 
+# Create a loss function
+# loss_fn = nn.BCELoss() # BCELoss = no sigmoid built-in
+loss_fn = nn.BCEWithLogitsLoss() # BCEWithLogitsLoss = sigmoid built-in
+
+# Create an optimizer
+optimizer = torch.optim.SGD(params=model_0.parameters(), 
+                            lr=0.1)
+
+# Calculate accuracy (a classification metric)
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc
 
 
+torch.manual_seed(42)
 
+# Set the number of epochs
+epochs = 1000
 
+# Build training and evaluation loop
+for epoch in range(epochs):
+    ### Training
+    model_0.train()
+
+    # 1. Forward pass (model outputs raw logits)
+    y_logits = model_0(X_train).squeeze() # squeeze to remove extra `1` dimensions, this won't work unless model and data are on same device 
+    y_pred = torch.round(torch.sigmoid(y_logits)) # turn logits -> pred probs -> pred labls
+  
+    # 2. Calculate loss/accuracy
+    # loss = loss_fn(torch.sigmoid(y_logits), # Using nn.BCELoss you need torch.sigmoid()
+    #                y_train) 
+    loss = loss_fn(y_logits, # Using nn.BCEWithLogitsLoss works with raw logits
+                   y_train) 
+    acc = accuracy_fn(y_true=y_train, 
+                      y_pred=y_pred) 
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backwards
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    ### Testing
+    model_0.eval()
+    with torch.inference_mode():
+        # 1. Forward pass
+        test_logits = model_0(X_test).squeeze() 
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        # 2. Caculate loss/accuracy
+        test_loss = loss_fn(test_logits,
+                            y_test)
+        test_acc = accuracy_fn(y_true=y_test,
+                               y_pred=test_pred)
+
+    # Print out what's happening every 10 epochs
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
+
+def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
+    # Setup prediction boundaries and grid
+    x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
+    y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101), np.linspace(y_min, y_max, 101))
+
+    # Make features
+    X_to_pred_on = torch.from_numpy(np.column_stack((xx.ravel(), yy.ravel()))).float()
+
+    # Make predictions
+    model.eval()
+    with torch.inference_mode():
+        y_logits = model(X_to_pred_on)
+        y_pred = torch.round(torch.sigmoid(y_logits))  # binary
+
+    # Reshape preds and plot
+    y_pred = y_pred.reshape(xx.shape).detach().numpy()
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    
+plot_decision_boundary(model_0,X_train, y_train)
+plt.show()
